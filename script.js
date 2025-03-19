f8c093912043276560ebfe7d6ad4fa9ce823b705
Carjardying <carolinejardy.pro@gmail.com>
2025-03-19 15:47:39 +0100

    getEvolution and setInterval started

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const decadeContainer = document.querySelector("#decadeContainer");
 const selectDecade = document.querySelector("#selectDecade");
-const buttonEvolution = document.querySelector('#buttonEvolution');
+const buttonEvolution = document.querySelector("#buttonEvolution");
+let intervalID;
 
 // const dateStart = [1970, 1980, 1990, 2000, 2010, 2020];
 // const dateEnd = [1979, 1989, 1999, 2009, 2019, 2029];
 
 const urlSeventies =
-   "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1970-01-01&primary_release_date.lte=1979-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1970-01-01&primary_release_date.lte=1979-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
 
 const urlEighties =
   "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1980-01-01&primary_release_date.lte=1989-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
@@ -23,9 +24,17 @@ const urlTwentyTenies =
 const urlTwentyTwenties =
   "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=2020-01-01&primary_release_date.lte=2029-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
 
+const urlArray = [
+  urlSeventies,
+  urlEighties,
+  urlNineties,
+  urlTwenies,
+  urlTwentyTenies,
+  urlTwentyTwenties,
+];
 
 /****SECONDARY FUNCTION******
-***************************/
+ ***************************/
 // function getUrl (dateStart, dateEnd){
 //  let urlDecade =
 //   `https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=${dateStart}-01-01&primary_release_date.lte=${dateEnd}-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a`;
@@ -33,23 +42,21 @@ const urlTwentyTwenties =
 // }
 
 function getEvolution(years) {
-
   let count = 0;
-  getDecade(getUrl(dateStart[count], dateEnd[count]), years)
-  while (count < dateStart.length) {
-    
-    count++;
+  getDecade(urlArray[count], years);
 
-    setTimeout(() => {
-      getDecade(getUrl(dateStart[count], dateEnd[count]), years)
-
+  if (count < urlArray.length) {
+    intervalID = setInterval(() => {
+      count++;
+      decadeContainer.innerHTML = "";
+      getDecade(urlArray[count], years);
     }, 5000);
   }
+  clearInterval(intervalID);
 }
 
-
 /****PRINCIPAL FUNCTION******
-****************************/
+ ****************************/
 async function getDecade(url, years) {
   try {
     const response = await fetch(url);
@@ -72,7 +79,7 @@ async function getDecade(url, years) {
 }
 
 /*******USER CHOICE*******
-**************************/
+ **************************/
 
 selectDecade.addEventListener("change", () => {
   decadeContainer.innerHTML = "";
@@ -102,6 +109,7 @@ selectDecade.addEventListener("change", () => {
   }
 });
 
-buttonEvolution.addEventListener('click', () => {
-  getEvolution("");
-})
+buttonEvolution.addEventListener("click", () => {
+  decadeContainer.innerHTML = "";
+  getEvolution();
+});
