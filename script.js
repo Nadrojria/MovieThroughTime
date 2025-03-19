fe79dd1107541b7199f2fb6285bb448edd96cfa6
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-19 14:06:49 +0100

    back to split url

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const decadeContainer = document.querySelector("#decadeContainer");
 const selectDecade = document.querySelector("#selectDecade");
+const buttonEvolution = document.querySelector('#buttonEvolution');
 
-const dateStart = [1970, 1980, 1990, 2000, 2010, 2020];
-const dateEnd = [1979, 1989, 1999, 2009, 2019, 2029];
+// const dateStart = [1970, 1980, 1990, 2000, 2010, 2020];
+// const dateEnd = [1979, 1989, 1999, 2009, 2019, 2029];
+
+const urlSeventies =
+   "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1970-01-01&primary_release_date.lte=1979-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
+const urlEighties =
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1980-01-01&primary_release_date.lte=1989-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
+const urlNineties =
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
+const urlTwenies =
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=2000-01-01&primary_release_date.lte=2009-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
+const urlTwentyTenies =
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=2010-01-01&primary_release_date.lte=2019-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
+const urlTwentyTwenties =
+  "https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=2020-01-01&primary_release_date.lte=2029-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a";
+
 
 /****SECONDARY FUNCTION******
 ***************************/
-function getUrl (dateStart, dateEnd){
- let urlDecade =
-  `https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=${dateStart}-01-01&primary_release_date.lte=${dateEnd}-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a`;
-  return urlDecade;
-}
+// function getUrl (dateStart, dateEnd){
+//  let urlDecade =
+//   `https://api.themoviedb.org/3/discover/movie?page=1&primary_release_date.gte=${dateStart}-01-01&primary_release_date.lte=${dateEnd}-12-31&sort_by=popularity.desc&vote_count.gte=5000&api_key=07d42f5abde4196c2595e75f9ee0975a`;
+//   return urlDecade;
+// }
 
 function getEvolution(years) {
+
   let count = 0;
+  getDecade(getUrl(dateStart[count], dateEnd[count]), years)
   while (count < dateStart.length) {
-    setInterval(getDecade(getUrl(dateStart[count], dateEnd[count]), years), 5000);
+    
     count++;
+
+    setTimeout(() => {
+      getDecade(getUrl(dateStart[count], dateEnd[count]), years)
+
+    }, 5000);
   }
-  
 }
 
 
@@ -28,7 +54,6 @@ async function getDecade(url, years) {
   try {
     const response = await fetch(url);
     const dataDecade = await response.json();
-    console.log(dataDecade);
 
     let movieList = document.createElement("ol");
     movieList.innerText = `Top 10 in the ${years}'s`;
@@ -54,29 +79,29 @@ selectDecade.addEventListener("change", () => {
   const decade = selectDecade.value;
 
   switch (decade) {
-    case "70's":
-      let fetchUrl = getUrl(dateStart[0], dateEnd[0]);
-      getDecade(fetchUrl, decade);
+    case "70":
+      getDecade(urlSeventies, decade);
       break;
-    case "80's":
-      getDecade(getUrl(dateStart[1], dateEnd[1]), decade);
+    case "80":
+      getDecade(urlEighties, decade);
       break;
-    case "90's":
-      getDecade(getUrl(dateStart[2], dateEnd[2]), decade);
+    case "90":
+      getDecade(urlNineties, decade);
       break;
-    case "00's":
-      getDecade(getUrl(dateStart[3], dateEnd[3]), decade);
+    case "00":
+      getDecade(urlTwenies, decade);
       break;
-    case "10's":
-      getDecade(getUrl(dateStart[4], dateEnd[4]), decade);
+    case "10":
+      getDecade(urlTwentyTenies, decade);
       break;
-    case "20's":
-      getDecade(getUrl(dateStart[5], dateEnd[5]), decade);
+    case "20":
+      getDecade(urlTwentyTwenties, decade);
       break;
-    // case "evolution":
-    //   //getEvolution(decade);
-    //   break;
     default:
       break;
   }
 });
+
+buttonEvolution.addEventListener('click', () => {
+  getEvolution("");
+})
